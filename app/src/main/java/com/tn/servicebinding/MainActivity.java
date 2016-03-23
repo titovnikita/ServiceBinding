6adda5d339a252b8c5d077b1064de6805fcfec37
nick <titovnikita1@gmail.com>
2016-03-23 22:49:48 +0200

    Added IPC, changing simple local binding with binder to interprocess handlers

--- app/src/main/java/com/tn/servicebinding/MainActivity.java
+++ app/src/main/java/com/tn/servicebinding/MainActivity.java
@@ -1,14 +1,16 @@
 package com.tn.servicebinding;
 
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
 import android.os.Messenger;
+import android.os.RemoteException;
 import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -16,7 +18,6 @@ import java.lang.ref.WeakReference;
 
 public class MainActivity extends ActionBarActivity {
 
-    private MyBindService service;
     private ServiceMessagesHandler serviceMessagesHandler;
     private Messenger messenger;
     private boolean isServiceConnected = false;
@@ -26,7 +27,6 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         serviceMessagesHandler = new ServiceMessagesHandler(this);
-        messenger = new Messenger(serviceMessagesHandler);
     }
 
     @Override
@@ -41,40 +41,53 @@ public class MainActivity extends ActionBarActivity {
         super.onStop();
         if (isServiceConnected) {
             unbindService(serviceConnection);
-            service = null;
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        serviceMessagesHandler = null;
     }
 
     private ServiceConnection serviceConnection = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
             Log.d(getClass().getSimpleName(), "ServiceConnection connected");
-            MyBindService.ServiceBinder binder = (MyBindService.ServiceBinder) iBinder;
-            service = binder.getService();
+            messenger = new Messenger(iBinder);
             isServiceConnected = true;
+
+            Message msg = Message.obtain(null, MyBindService.OPERATION_START);
+            msg.replyTo = new Messenger(serviceMessagesHandler);
+            try {
+                messenger.send(msg);
+            } catch (RemoteException ex) {
+                ex.printStackTrace();
+            }
         }
 
         @Override
         public void onServiceDisconnected(ComponentName componentName) {
             Log.d(getClass().getSimpleName(), "ServiceConnection disconnected");
             isServiceConnected = false;
+            messenger = null;
         }
     };
 
     private static class ServiceMessagesHandler extends Handler {
-        private final WeakReference<MainActivity> myClassWeakReference;
+        private final WeakReference<Context> contextWeakReference;
 
-        public ServiceMessagesHandler(MainActivity myClassInstance) {
-            myClassWeakReference = new WeakReference<MainActivity>(myClassInstance);
+        public ServiceMessagesHandler(Context context) {
+            contextWeakReference = new WeakReference<Context>(context);
         }
 
         @Override
         public void handleMessage(Message msg) {
-            MainActivity reference = myClassWeakReference.get();
+            Context reference = contextWeakReference.get();
             if (reference != null) {
                 switch (msg.what) {
                     case MyBindService.OPERATION_FINISHED:
-                        Toast.makeText(reference, "Operation finished." + reference.service.getNumber(), Toast.LENGTH_SHORT).show();
+                        Toast.makeText(reference, "Operation finished.", Toast.LENGTH_SHORT).show();
                         break;
                     default:
                         super.handleMessage(msg);
--- app/src/main/java/com/tn/servicebinding/MyBindService.java
+++ app/src/main/java/com/tn/servicebinding/MyBindService.java
@@ -1,27 +1,76 @@
 package com.tn.servicebinding;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Binder;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
+import android.widget.Toast;
+
+import java.lang.ref.WeakReference;
 
 public class MyBindService extends Service {
-    public static final int OPERATION_FINISHED = 1;
+    public static final int OPERATION_START = 1;
+    public static final int OPERATION_FINISHED = 2;
 
-    private final IBinder binder = new ServiceBinder();
+    private Messenger messenger;
+    private MessageHandler msgHandler;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        msgHandler = new MessageHandler(this);
+        messenger = new Messenger(msgHandler);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        msgHandler = null;
+        messenger = null;
+    }
 
     @Override
     public IBinder onBind(Intent intent) {
-        return binder;
+        return messenger.getBinder();
     }
 
-    public class ServiceBinder extends Binder {
-        MyBindService getService() {
-            return MyBindService.this;
+    private static class MessageHandler extends Handler {
+        private final WeakReference<Context> contextWeakReference;
+
+        public MessageHandler(Context context) {
+            contextWeakReference = new WeakReference<Context>(context);
+        }
+
+        @Override
+        public void handleMessage(final Message msg) {
+            Context reference = contextWeakReference.get();
+            if (reference != null) {
+                switch (msg.what) {
+                    case OPERATION_START:
+                        Toast.makeText(reference, "Operation started.", Toast.LENGTH_SHORT).show();
+                        new Handler().postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                try {
+                                    Message message = Message.obtain(null, OPERATION_FINISHED);
+                                    msg.replyTo.send(message);
+                                } catch (RemoteException ex) {
+                                    ex.printStackTrace();
+                                }
+                            }
+                        }, 4000);
+                        break;
+                    default:
+                        super.handleMessage(msg);
+                        break;
+                }
+            }
         }
     }
 
-    public int getNumber() {
-        return 10;
-    }
 }
